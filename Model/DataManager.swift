f3777e4c2316de3eebddb0d7a2c073d85e5392e8
Ivan Redreev <redreev.ivan@gmail.com>
2020-09-15 13:04:35 +0500

    LoginScreen

---
+++ Model/DataManager.swift
@@ -0,0 +1,19 @@
+//
+//  DataManager.swift
+//  CommandApp
+//
+//  Created by Rasikon on 14.09.2020.
+//  Copyright © 2020 Rasikon. All rights reserved.
+//
+
+import Foundation
+
+class DataManager {
+    
+    static let data = DataManager()
+    
+    private init() {}
+    
+    var persons = [Person]()
+    
+}
---
+++ ViewControllers/LoginViewController.swift
@@ -0,0 +1,10 @@
+//
+//  LoginViewController.swift
+//  LoginApp
+//
+//  Created by Alexey Efimov on 19/06/2019.
+//  Copyright © 2019 Alexey Efimov. All rights reserved.
+//
+
+
+
